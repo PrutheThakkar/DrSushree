21354fa6f8d775d4019ff0fb3e65a8cfd39ae42f
Pruthe Thakkar <pruthe.thakkar@wockhardthospitals.com>
2026-04-16 13:13:28 +0530

    fixed layout file

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,8 @@
 // src/layouts/index.js
 
 import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from './Header'
+import Footer from './Footer'
 
 import "../css/common.css"
 import "../css/home.css"
