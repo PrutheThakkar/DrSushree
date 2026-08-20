25dc5bd527d68432a9c828776d11b631836fb327
Pruthe Thakkar <pruthe.thakkar@wockhardthospitals.com>
2026-08-20 12:35:04 +0530

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,29 +2,37 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: "Dr sushree",
-    description: "Gatsby + WordPress (WPGraphQL) site",
-    author: "@Dr sushree",
+    title: "Dr. Sushree Patra",
+    description: "Dr. Sushree Patra - Obstetrician and Gynaecologist",
+    author: "@DrSushreePatra",
+
     siteUrl:
       process.env.GATSBY_WEBSITE_URL ||
-      "https://drsushree.studiosentientdemo.com",
+      "https://drsushreepatra.com",
   },
+
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sass",
+
+    // Generate sitemap
+    "gatsby-plugin-sitemap",
+
     {
       resolve: "gatsby-source-wordpress",
       options: {
         url:
           process.env.GATSBY_WPGRAPHQL_URL ||
           "https://drsushree.studiosentientdemo.com/graphql",
+
         develop: {
           hardCacheMediaFiles: true,
           hardCacheData: false,
           nodeUpdateInterval: 300000,
         },
+
         production: {
           hardCacheMediaFiles: false,
         },
